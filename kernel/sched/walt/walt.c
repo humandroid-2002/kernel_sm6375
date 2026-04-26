9b1595ccd3b66adc2bbdf4f6e03796d456d9cf61
Yama <pitbike216@gmail.com>
2026-04-26 17:37:15 +0200

    Tweak WALT values for better core utilization

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -2326,7 +2326,7 @@ void mark_task_starting(struct task_struct *p)
  * sched_group_upmigrate need to be up-migrated if possible.
  */
 unsigned int __read_mostly sched_group_upmigrate = 20000000;
-unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
+unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 98;
 
 /*
  * Task groups, once up-migrated, will need to drop their aggregate
@@ -2334,7 +2334,7 @@ unsigned int __read_mostly sysctl_sched_group_upmigrate_pct = 100;
  * migrated.
  */
 unsigned int __read_mostly sched_group_downmigrate = 19000000;
-unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 95;
+unsigned int __read_mostly sysctl_sched_group_downmigrate_pct = 91;
 
 static inline void walt_update_group_thresholds(void)
 {
